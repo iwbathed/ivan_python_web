a4b04ebbde043ca0e6f8aa9ca248f0c14052fb31
Iwba <ivankoprogamer@gmail.com>
2022-11-10 01:00:51 +0200

    add fields to bd, add html, changed views

--- lab5/app/models.py
+++ lab5/app/models.py
@@ -5,6 +5,9 @@ class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(20), unique=False, nullable=False)
     email = db.Column(db.String(225), unique=True, nullable=False)
+    phone = db.Column(db.String(15), unique=True, nullable=False)
+    subject = db.Column(db.String(30), unique=False, nullable=False)
+    message = db.Column(db.Text, unique=False, nullable=False)
 
     def repr(self):
-        return f"Name : {self.name}, Email: {self.email}"
+        return f"Name : {self.name}, Email: {self.email}, Phone: {self.phone}, Subject: {self.subject},  Message: {self.message}"
--- lab5/app/views.py
+++ lab5/app/views.py
@@ -1,8 +1,6 @@
 
 # -*- coding: UTF-8 -*-
-"""
-hello_flask: First Python-Flask webapp
-"""
+
 import platform
 from datetime import datetime
 
@@ -16,8 +14,6 @@ from app.models import Person
 logging.basicConfig(filename='app.log', filemode='a',
                     format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
-
-#app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'SSSqwed453rvsdf4df'
 
@@ -37,7 +33,6 @@ contact_l = {
 
 
 
-
 navs = [
     {
         "name" : "Home",
@@ -54,18 +49,21 @@ navs = [
     {
         "name": "Projects",
         "url": "projects"
+    },
+    {
+        "name": "Persons info",
+        "url": "person_info"
     }
 ]
 
 
-
-
 def footer_l_get():
-
     return [platform.platform(),
               request.headers.get('User-Agent'),
               datetime.now()
               ]
+
+
 @app.route('/')
 def start():
     return redirect("/home")
@@ -77,71 +75,100 @@ def home():
                            navs=navs,
                            footer_l=footer_l_get())
 
+
 @app.route('/about')
 def about():
     return render_template('about.html',
                            navs=navs,
                            footer_l=footer_l_get())
 
+
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     form = Form()
-    if request.method == 'GET':
-        return render_template('contact.html', navs=navs,
-                               contact_l=contact_l,
-                               footer_l=footer_l_get(),
-                               form=form,
-                               session=session)
-    if form.validate_on_submit():
-        username = form.name.data
-        email = form.email.data
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            username = form.name.data
+            email = form.email.data
 
-        session['name'] = username
-        session['email'] = email
+            phone = form.phone.data
+            subject = form.subject.data
+            message = form.message.data
 
-        contact_info = Person(name=username, email=email)
-        db.session.add(contact_info)
-        db.session.commit()
+            session['name'] = username
+            session['email'] = email
 
-        form.name.data = 'Successfull validate '
-        flash(f"Data sent successfully : {username} {email}",
-              category='success')
-        logging.info(f"Data sent successfully : {username} {email}")
+            contact_info = Person(name=username, email=email,
+                                  phone=phone, subject=subject,
+                                  message=message
+                                  )
+            db.session.add(contact_info)
+            db.session.commit()
+
+            form.name.data = 'Successfull validate '
+            flash(f"Data sent successfully : {username} {email} "
+                  f"{phone} {subject} {message}",
+                  category='success')
+            logging.info(f"Data sent successfully : {username} {email} "
+                         f"{phone} {subject} {message}")
+            return redirect(url_for("contact"))
+        flash("Not validate (Post)", category='warning')
+        logging.warning("Not validate (Post)")
         return redirect(url_for("contact"))
+    return render_template('contact.html', navs=navs,
+                           contact_l=contact_l,
+                           footer_l=footer_l_get(),
+                           form=form,
+                           session=session)
+
+
 
     flash("Not validate (Post)", category='warning')
     logging.warning("Not validate (Post)")
     return redirect(url_for("contact"))
 
 
-
 @app.route('/projects')
 def projects():
-
     return render_template('projects.html',
                            navs=navs,
                            footer_l=footer_l_get())
 
-@app.route('/person_info', methods=['GET'])
-def person_info():
-    user_contact_id = request.args.get('id')
-    if user_contact_id:
-        contact_info_list = Person.query.filter_by(id=user_contact_id)
-    else:
-        contact_info_list = Person.query.all()
-    return render_template('person.html', contact_info_list=contact_info_list)
 
+
+@app.route('/person_info', methods=['GET'])
+def persons_info():
+    contact_info_list = Person.query.all()
+    return render_template('person.html',
+                           contact_info_list=contact_info_list,
+                           navs=navs,
+                           footer_l=footer_l_get()
+                           )
+
+
+@app.route('/person_info/<id>', methods=['GET'])
+def person_info(id):
+    contact_info_list = Person.query.filter_by(id=id)
+    return render_template('person.html',
+                           contact_info_list=contact_info_list,
+                           navs=navs,
+                           footer_l=footer_l_get()
+                           )
 
 @app.route('/person/delete/<id>')
 def delete_person(id):
-    Person.query.filter_by(id=id).delete()
+
     try:
+        Person.query.filter_by(id=id).delete()
         db.session.commit()
     except:
         db.session.flush()
         db.session.rollback()
-    return "<h3>Deleted!</h3>"
-
+    return render_template('on_delete.html',
+                           id=id,
+                           navs=navs,
+                           footer_l=footer_l_get()
+                           )
 
 
 if __name__ == '__main__':
